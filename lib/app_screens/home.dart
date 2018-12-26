7b3f17aa1f4797b593fadf6a25affb67d122a6be
Maulik Dhameliya <maulik127@gmail.com>
2018-12-26 17:09:38 +0530

    apply font

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -5,22 +5,102 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Container(
-          alignment: Alignment.center,
-          color: Colors.deepOrange,
+            alignment: Alignment.center,
+            color: Colors.deepOrange,
+            padding: EdgeInsets.only(left:15.0,top: 40.0),
 //        width: 200.0,
 //        height: 100.0,
-          //margin: EdgeInsets.only(left:15.0,top: 15.0),
-          child: Text(
-              "Maulik",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                fontSize: 80.0,
-                  fontFamily: 'Raleway',
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w700,
-                  decoration: TextDecoration.none, color: Colors.white)
-          ),
+            //margin: EdgeInsets.only(left:15.0,top: 15.0),
+            child: Column(
+              children: <Widget>[
+                Row(children: <Widget>[Expanded(
+                    child: Text(
+                        "Neoyug",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            fontSize: 35.0,
+                            fontFamily: 'Raleway',
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w700,
+                            decoration: TextDecoration.none,
+                            color: Colors.white)
+                    )
+                ), Expanded(
+                    child: Text(
+                        "Tech",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            fontSize: 35.0,
+                            fontFamily: 'Raleway',
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w700,
+                            decoration: TextDecoration.none,
+                            color: Colors.white)
+                    )
+                ),
+                ],),
+                Row(children: <Widget>[Expanded(
+                    child: Text(
+                        "Neoyug",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            fontSize: 35.0,
+                            fontFamily: 'Raleway',
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w700,
+                            decoration: TextDecoration.none,
+                            color: Colors.white)
+                    )
+                ), Expanded(
+                    child: Text(
+                        "Tech",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            fontSize: 35.0,
+                            fontFamily: 'Raleway',
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w700,
+                            decoration: TextDecoration.none,
+                            color: Colors.white)
+                    )
+                ),
+                ],),
+                ImageAsset(),
+                Button()
+              ],
+            )
+
+
         )
+    );
+  }
+
+}
+
+class ImageAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/download.jpeg');
+    Image image = Image(image: assetImage, width: 200, height: 100,);
+    return Container(child: image,);
+  }
+
+}
+
+class Button extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      width: 250.0,
+        height: 50.0,
+      child: RaisedButton(
+          onPressed: (){},
+        color: Colors.green,
+        elevation: 6.0,
+        child: Text("HELLO NY TECH",
+        style: TextStyle(color: Colors.white, fontSize: 20.0, fontFamily: 'Raleway', fontWeight: FontWeight.w700),),
+      ),
     );
   }
 
