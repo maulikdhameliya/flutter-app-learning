5544be9c67e4844419c68e52121c3aa86879f1b9
Maulik Dhameliya <maulik127@gmail.com>
2018-12-26 16:35:45 +0530

    apply font

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -4,16 +4,24 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        alignment: Alignment.center,
-        color: Colors.deepOrange,
+        child: Container(
+          alignment: Alignment.center,
+          color: Colors.deepOrange,
 //        width: 200.0,
 //        height: 100.0,
-      //margin: EdgeInsets.only(left:15.0,top: 15.0),
-        child: Text("Maulik", textDirection: TextDirection.ltr,),
-      )
+          //margin: EdgeInsets.only(left:15.0,top: 15.0),
+          child: Text(
+              "Maulik",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontSize: 80.0,
+                  fontFamily: 'Raleway',
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w700,
+                  decoration: TextDecoration.none, color: Colors.white)
+          ),
+        )
     );
-
   }
 
 }
